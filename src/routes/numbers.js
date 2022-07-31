2bde6ac0709123bd85fc2a1d3114e8759de65f2a
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-07-31 15:45:14 +0100

    reformat

--- src/routes/numbers.js
+++ src/routes/numbers.js
@@ -3,12 +3,14 @@ const { add } = require('../lib/numbers');
 
 const router = Router();
 
-router.get('/add/:number1/and/:number2', (req, res) => {
-  const { number1, number2 } = req.params;
-  if (Number.isNaN(Number(number1)) || Number.isNaN(Number(number2))) {
-    return res.status(400).json({ error: 'Parameters must be valid numbers.' });
-  }
-  return res.status(200).json({ result: add(+number1, +number2) });
+router
+  .get('/add/:number1/and/:number2', (req, res) => {
+    const { number1, number2 } = req.params;
+    if (Number.isNaN(Number(number1)) || Number.isNaN(Number(number2))) {
+      return res.status(400).json({ error: 'Parameters must be valid numbers.' });
+    }
+    return res.status(200).json({ result: add(+number1, +number2) });
+  })
 });
 
 module.exports = router;
