67988602627d47eed08e7078069ce1a3b6876998
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-01 13:05:44 +0100

    POST /arrays/to-string returns the stringified array

--- src/routes/arrays.js
+++ src/routes/arrays.js
@@ -4,10 +4,15 @@ const { getNthElement, arrayToCSVString } = require('../lib/arrays');
 
 const router = Router();
 
-router.post('/element-at-index/:index', (req, res) => {
-  const { index } = req.params;
-  const { array } = req.body;
-  return res.status(200).json({ result: getNthElement(index, array) });
-});
+router
+  .post('/element-at-index/:index', (req, res) => {
+    const { index } = req.params;
+    const { array } = req.body;
+    return res.status(200).json({ result: getNthElement(index, array) });
+  })
+  .post('/to-string', (req, res) => {
+    const { array } = req.body;
+    return res.status(200).json({ result: arrayToCSVString(array) });
+  });
 
 module.exports = router;
