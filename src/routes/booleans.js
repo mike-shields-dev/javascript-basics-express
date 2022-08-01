bfd0c7b9d141dff224119b00c564cc34be313932
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-08-01 12:26:56 +0100

    GET /booleans/{string}/starts-with/{character} passes all tests

--- src/routes/booleans.js
+++ src/routes/booleans.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 
-const { negate, truthiness, isOdd } = require('../lib/booleans');
+const { negate, truthiness, isOdd, startsWith } = require('../lib/booleans');
 
 const router = Router();
 
@@ -8,6 +8,14 @@ function requireParamIsNumber(req, res, next) {
   const { value } = req.params;
   if (Number.isNaN(Number(value))) {
     return res.status(400).json({ error: 'Parameter must be a number.' });
+  }
+  return next();
+}
+
+function requireParamIsChar(req, res, next) {
+  const { character } = req.params;
+  if (character.length !== 1) {
+    return res.status(400).json({ error: 'Parameter "character" must be a single character.' });
   }
   return next();
 }
@@ -24,6 +32,10 @@ router
   .get('/is-odd/:value', requireParamIsNumber, (req, res) => {
     const { value } = req.params;
     return res.status(200).json({ result: isOdd(value) });
+  })
+  .get('/cat/starts-with/:character', requireParamIsChar, (req, res) => {
+    const { character } = req.params;
+    return res.status(200).json({ result: startsWith(character, 'cat') });
   });
 
 module.exports = router;
